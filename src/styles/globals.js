110117a35b3e19e868105891c1c1dbe0f884ec67
davidponc <buap.david@gmail.com>
2021-01-19 18:06:33 -0600

    Initial commit

---
+++ src/styles/globals.js
@@ -0,0 +1,20 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+  html,
+  body {
+    padding: 0;
+    margin: 0;
+    font-family: 'Inter', system-ui, sans-serif;
+  }
+  
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+  
+  * {
+    box-sizing: border-box;
+    margin: 0;
+  }
+`
