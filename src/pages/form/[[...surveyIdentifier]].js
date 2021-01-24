c76bc86b8eeafee5ecea1fcd0df8c2187e6f5a32
davidponc <buap.david@gmail.com>
2021-01-24 12:24:24 -0600

    SUR-27 Feature: create form client view (initial version)

---
+++ src/pages/form/[[...surveyIdentifier]].js
@@ -0,0 +1,9 @@
+import Layout from 'components/Layout'
+
+export default function Form () {
+  return (
+    <Layout title="creator" design="default">
+      <h1>Form client view</h1>
+    </Layout>
+  )
+}
